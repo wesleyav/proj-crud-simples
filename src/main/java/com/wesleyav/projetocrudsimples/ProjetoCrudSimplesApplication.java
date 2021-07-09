479ae297eaa8359e1d61fb3c7e8c1bf69ce89b8a
Wesley Araujo Vieira <wesley.contato@hotmail.com>
2021-07-08 21:55:59 -0300

    Adicionado arquivos do projeto

---
+++ src/main/java/com/wesleyav/projetocrudsimples/ProjetoCrudSimplesApplication.java
@@ -0,0 +1,13 @@
+package com.wesleyav.projetocrudsimples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoCrudSimplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoCrudSimplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wesleyav/projetocrudsimples/ProjetoCrudSimplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wesleyav.projetocrudsimples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoCrudSimplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
